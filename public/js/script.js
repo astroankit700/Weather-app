d6a9ed61c6244348643cf8d3f262e7ab435f0289
astroankit700 <ankitagrawal700aa@gmail.com>
2021-10-13 01:12:02 +0530

    Improved details and help section

--- public/js/script.js
+++ public/js/script.js
@@ -18,30 +18,43 @@
 
 // })})
 console.log('heya man');
-const weatherForm=document.querySelector('form');
-const search=document.querySelector('input');
-const locationElement=document.querySelector('.location')
-const describeElement=document.querySelector('.describe')
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const locationElement = document.querySelector('.location');
+const describeElement = document.querySelector('.describe');
+const descriptionImage = document.querySelector('.description-image');
+const humidity = document.querySelector('.humidity');
+const wind = document.querySelector('.wind');
 
-locationElement.innerHTML='<font color="#888">Your results will appear here...</font>'
+locationElement.innerHTML =
+    '<font color="#888">Your results will appear here...</font>';
 
-weatherForm.addEventListener('submit',(e)=>{
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const location=search.value;
-    locationElement.innerHTML='<font color="#888">Loading...</font>'
-    describeElement.innerHTML="";
+    const location = search.value;
+    locationElement.innerHTML = '<font color="#888">Loading...</font>';
+    describeElement.innerHTML = '';
+    humidity.innerHTML=''
+    wind.innerHTML=''
 
-
-    fetch('/weather?address='+location).then((response)=>{
-        response.json().then((data)=>{
-            if(data.error) {
-                locationElement.textContent=data.error;
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                locationElement.textContent = data.error;
                 return console.log(`Error: ${data.error}`);
             }
+
             console.log(data);
-            locationElement.innerHTML=data.location;
-            describeElement.innerHTML=data.describe+`. It's ${data.temperature} degrees out and feels like ${data.feelslike} degree.`
-        })
-    })
+            locationElement.innerHTML = data.location;
+            describeElement.innerHTML =
+                '<img src='+data.weathericon+' alt="f" class="description-image"></img>' +
+                `<span class="temp"> ${data.temperature}<sup>0</sup>C</span>`+
+                `<p><h2>${data.describe}.</h2><br> It's ${data.temperature} degrees out and feels like ${data.feelslike} degree.<p>`;
+                humidity.innerHTML=`Humidity outside is ${data.humidity}.`
+                wind.innerHTML=`Wind is blowing at a speed of ${data.wind_speed}km/hr.`
+            // descriptionImage.style.display = 'block';
+            // console.log(descriptionImage);
+        });
+    });
     weatherForm.reset();
-}) 
+});
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,6 @@
 const request = require('postman-request');
 
-const forecast = ({latitude,longitude,location}, callback) => {
+const forecast = ({ latitude, longitude, location }, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=355be30bc7c3844ee5f4a3f5fe1aab29&query=${latitude},${longitude}&units=m`;
     request({ url, json: true }, (error, response) => {
         if (error) {
@@ -9,12 +9,18 @@ const forecast = ({latitude,longitude,location}, callback) => {
             callback(response.body.error.info, undefined);
         } else {
             let current = response.body.current;
-            let {temperature,feelslike} =current;
+            let weathericon = current.weather_icons[0];
+            let { temperature, feelslike, humidity, precip, wind_speed } =
+                current;
             callback(null, {
                 location,
                 describe: current.weather_descriptions[0],
                 temperature,
                 feelslike,
+                humidity,
+                precip,
+                weathericon,
+                wind_speed,
             });
         }
     });
